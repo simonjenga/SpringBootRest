96c01d34a5614720b64d94dff262ddb9371b6510
Simon N <simtuje@gmail.com>
2016-01-22 18:35:58 +0300

    more javadocs updates

--- src/main/java/com/spring/boot/rest/domain/DomainObject.java
+++ src/main/java/com/spring/boot/rest/domain/DomainObject.java
@@ -9,6 +9,21 @@ import java.io.Serializable;
  * @version 0.1
  */
 public interface DomainObject extends Serializable {
-    
+
+    /**
+     * Returns the primary key identifier.
+     *
+     * @return id Primary key identifier.
+     */
     Long getId();
+
+    /**
+     * Sets the primary key identifier.
+     *
+     * WARNING: Setting the identifier is usually delegated to the database.
+     * Implementations may throw an exception if you attempt to set this value manually.
+     *
+     * @param <code>Long</code>id, the Primary key identifier to set.
+     */
+    void setId(Long id);
 }
