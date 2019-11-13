8517df1c94870d3df9bca0a0e2b0e6dd02941141
simonjenga <simtuje@gmail.com>
2019-11-13 13:10:00 +0300

    refactorings on java source files

--- src/main/java/com/spring/boot/rest/controller/IndexController.java
+++ src/main/java/com/spring/boot/rest/controller/IndexController.java
@@ -1,7 +1,7 @@
 package com.spring.boot.rest.controller;
 
 //import org.springframework.boot.autoconfigure.web.ErrorController;
-// import org.springframework.boot.autoconfigure.web.servlet.error.ErrorController;
+//import org.springframework.boot.autoconfigure.web.servlet.error.ErrorController;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
