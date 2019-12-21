bf62702232ca88066cce2dcade70b9bb04771ade
simonjenga <simtuje@gmail.com>
2019-12-21 20:50:00 +0300

    refactorings on java source files

--- src/main/java/com/spring/boot/rest/Application.java
+++ src/main/java/com/spring/boot/rest/Application.java
@@ -2,21 +2,27 @@ package com.spring.boot.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * This is the main entry class to this Project/Application.
  * It is an ordinary java class with a main method {@link #main(String[])}
  * 
+ * -> /api/org/springframework/boot/context/web/SpringBootServletInitializer.html
+ * 
  * @author Simon Njenga
  * @since 0.1
  */
 @Configuration
-@EnableAutoConfiguration
 @ComponentScan
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableTransactionManagement
 public class Application extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
